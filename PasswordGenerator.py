1170617072f41696a867a1c5294e837c79c42619
Psimmons <pjs@cesimmons.com>
2024-03-14 13:30:57 -0400

    User Password input HIden - Run in Terminal

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,30 +3,10 @@
 #Program: Password Generator
 #Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
-import hashlib
-import os
+import getpass
 
-def generate_salt():
-    # Generate a random salt
-    return os.urandom(16)
+# Prompt the user for a password without showing the input characters
+password = getpass.getpass("Enter your password: ")
 
-def hash_password(password, salt):
-    # Hash the password with the salt using SHA-256 algorithm
-    hashed_password = hashlib.sha256(password.encode() + salt).hexdigest()
-    return hashed_password
-
-def main():
-    # Accept password input from the user
-    password = input("Enter your password: ")
-
-    # Generate a random salt
-    salt = generate_salt()
-
-    # Hash the password with the salt
-    hashed_password = hash_password(password, salt)
-
-    # Print the hashed password
-    print("Hashed Password:", hashed_password)
-
-if __name__ == "__main__":
-    main()
+# You can then use the password variable however you need
+print("Your password is:", password)
