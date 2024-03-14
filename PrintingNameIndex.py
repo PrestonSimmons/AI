27f6b59ff4dccd203ff3473f51bb43c6e3f6e6b6
Psimmons <pjs@cesimmons.com>
2024-03-14 14:03:30 -0400

    Pre Chat GPT Printing Names Index - Stable

--- PrintingNameIndex.py
+++ PrintingNameIndex.py
@@ -3,5 +3,12 @@
 #Program: Printing Names Index
 #Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
+names = ["Corey","Chris","Travis","Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
+
 
 
